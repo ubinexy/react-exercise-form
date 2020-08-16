aa16398757e9dc73c2501d1c30027defcd1fab4c
ubinexy <ubinexy@gmail.com>
2020-08-16 11:49:44 +0800

    feat: add submit check

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,23 +3,59 @@ import './myProfile.less';
 
 class MyProfile extends Component {
   state = {
-    name: "",
-    gender: ""
+    name: undefined,
+    gender: 'male',
+    desc: undefined,
+    checked: undefined
   }
 
-  submitForm = (event)=> {
-    event.target.value
-  };
 
-  handleChange = (event)=> {
+  handleNameChange = (event)=> {
     this.setState({
-      name: event.target.value
+      name: event.target.value,
     });
+  }
+
+  handleDescChange = (event)=> {
+    this.setState({
+      desc: event.target.value,
+    });
+  }
+
+  handleGenderChange = (event)=> {
+    this.setState({
+      gender: event.target.value,
+    });
+  }
+
+  handleCheckChange = (event)=> {
+    this.setState({
+      checked: event.target.checked,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(this.state.name === undefined) {
+      alert("Name is undefined")
+      return;
+    }
+    if(this.state.desc === undefined) {
+      alert("Description is undefined")
+      return;
+    }
+    if(this.state.checked === undefined) {
+      alert("You have to read terms of conduct first")
+      return;
+    }
+    alert("Success, check the console log.")
+    console.log(this.state);
   }
 
   render() {
     return (
-      <form onSubmit={this.submitForm}>
+      <form onSubmit={this.handleSubmit}>
         <label htmlFor="name">Name</label>
         <input 
           type="text" 
@@ -27,20 +63,21 @@ class MyProfile extends Component {
           name="name"
           placeholder="Your name"
           value={this.state.name}
+          onChange={this.handleNameChange}
           />
         <label htmlFor="gender">Gender</label>
-        <select name="gender" >
+        <select name="gender" value={this.state.gender} onChange={this.handleGenderChange}>
           <option value="male">Male</option>
           <option value="female">Female</option> 
         </select>
         <label htmlFor="desc">Description</label>
         <textarea cols="29" rows="5" name= "desc" 
-        placeholder="Description about yourself"></textarea>
+        placeholder="Description about yourself"  value={this.state.desc} onChange={this.handleDescChange}></textarea>
         <div>
-          <input type="checkbox" name="confirmed" value=""/>
+          <input type="checkbox" name="confirmed" value={this.state.checked} onChange={this.handleCheckChange}/>
           <label htmlFor="confirmed" id="label">I have read terms of conducts</label>
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" >Submit</button>
       </form>
     );
   }
